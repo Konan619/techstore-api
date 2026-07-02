b3f13bfcc5fce094ea6617edbe2341ea05ebe187
Konan619 <ax.blanlot@duocuc.cl>
2026-07-02 01:16:45 -0400

    Corrige package del test Spring Boot

--- src/test/java/techstore_api/TechstoreApplicationTests.java
+++ src/test/java/techstore_api/TechstoreApplicationTests.java
@@ -1,13 +1,12 @@
-package techstore_api;
+package cl.techstore.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TechstoreApplication.class)
 class TechstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
